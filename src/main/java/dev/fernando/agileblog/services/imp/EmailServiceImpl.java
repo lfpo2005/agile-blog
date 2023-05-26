9391483b7a89fac9ee16aa5ef5015e200dff51b7
lfpo2005 <lfpo2005@yahoo.com.br>
2023-05-26 11:01:18 -0300

    correção email

--- src/main/java/dev/fernando/agileblog/services/imp/EmailServiceImpl.java
+++ src/main/java/dev/fernando/agileblog/services/imp/EmailServiceImpl.java
@@ -6,8 +6,6 @@ import dev.fernando.agileblog.models.EmailModel;
 import dev.fernando.agileblog.repositories.EmailRepository;
 import dev.fernando.agileblog.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,13 +16,8 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
-import javax.mail.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -44,21 +37,37 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public EmailModel sendEmail(EmailModel emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(emailModel.getEmailFrom());
-            message.setTo(emailModel.getEmailTo());
-            message.setSubject(emailModel.getSubject());
-            message.setText(emailModel.getText());
-            emailSender.send(message);
+        OkHttpClient client = new OkHttpClient();
 
-            emailModel.setStatusEmail(StatusEmail.SENT);
-        } catch (MailException e) {
+        String json = "{"
+                + "\"subject\": \"" + emailModel.getSubject() + "\","
+                + "\"body\": \"" + emailModel.getText() + "\","
+                + "\"from\": \"" + emailModel.getEmailFrom() + "\","
+                + "\"to\": \"" + emailModel.getEmailTo() + "\","
+                + "\"headers\": {\"Content-Type\": \"text/html\"}"
+                + "}";
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .header("x-auth-token", API_TOKEN)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                emailModel.setStatusEmail(StatusEmail.SENT);
+            } else {
+                emailModel.setStatusEmail(StatusEmail.ERROR);
+            }
+        } catch (IOException e) {
             emailModel.setStatusEmail(StatusEmail.ERROR);
         } finally {
             return emailRepository.save(emailModel);
         }
     }
+
 /*
 
     @Override
