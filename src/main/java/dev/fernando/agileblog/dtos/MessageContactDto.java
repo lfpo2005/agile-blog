d44bae62faf0b7bc13d9e0e549f244f5397cee0c
lfpo2005 <lfpo2005@yahoo.com.br>
2023-06-06 11:41:58 -0300

    test final R-1.0.0

--- src/main/java/dev/fernando/agileblog/dtos/MessageContactDto.java
+++ src/main/java/dev/fernando/agileblog/dtos/MessageContactDto.java
@@ -3,6 +3,7 @@ package dev.fernando.agileblog.dtos;
 import dev.fernando.agileblog.enums.AnsweredType;
 import lombok.Data;
 
+import javax.persistence.Column;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -10,8 +11,12 @@ import java.util.UUID;
 public class MessageContactDto {
     private UUID messageContactId;
     private String message;
+    private String name;
+    private String userAnswer;
     private String email;
     private AnsweredType answered;
     private boolean visualized;
     private LocalDateTime creationDate;
+    private LocalDateTime dateAnswer;
+
 }
